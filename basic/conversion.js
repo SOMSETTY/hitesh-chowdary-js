09dc3b59a157edd2437fbeebbaf80898ea429de4
sandeep kumar <somsettysandeep@gmail.com>
2024-05-12 17:41:02 +0530

    conversion

---
+++ basic/conversion.js
@@ -0,0 +1,40 @@
+let score = "sandeep"
+
+
+
+// console.log(typeof score)
+
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+/*
+conversion types    
+ "33"  => 33
+ "33abc" => nan - not a number
+ "true "=> 1 "false" => 0
+ 1=> true :
+ 0=> false :
+ ""=> false 
+ "sandeep"=> true
+*/
+ let isLoggedIn =  "sandeep"
+
+ console.log(isLoggedIn);
+
+
+ let booleanIsloggedIn = Boolean(isLoggedIn)
+//  console.log(booleanIsloggedIn)
+
+
+//  let someNumber = 33;
+//  let stringNumber = string(someNumber);
+//  console.log(stringNumber);
+
+ let someNumber = 33;
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof(stringNumber));
+
