955a5ca534acc914962da32f2b42199aad509680
sandeep kumar <somsettysandeep@gmail.com>
2024-05-12 18:48:44 +0530

    operation types

--- basic/conversion.js
+++ basic/conversion.js
@@ -22,7 +22,7 @@ conversion types
 */
  let isLoggedIn =  "sandeep"
 
- console.log(isLoggedIn);
+//  console.log(isLoggedIn);
 
 
  let booleanIsloggedIn = Boolean(isLoggedIn)
@@ -35,6 +35,48 @@ conversion types
 
  let someNumber = 33;
 let stringNumber = String(someNumber);
-console.log(stringNumber);
-console.log(typeof(stringNumber));
+// console.log(stringNumber);
+// console.log(typeof(stringNumber));
+
+
+//  *************** operation  ************
+
+let value  = 3 ;
+let negValue = -value
+// console.log(negValue)
+
+// 
+let str1= "hello";
+let str2= "hitesh";
+
+let str3 = str1 + str2
+console.log(str3);
+
+
+console.log("1"+2); //12
+console.log(1+"2"); //12
+console.log ("1"+"2");//12
+console.log ("1"+2+2); //122
+console.log (1+2+"2");//32
+
+console.log((3+4)*5 % 3);
+
+console.log(true)
+
+
+let num1 , num2, num3
+
+num1= num2= num3 = 2+ 2
+
+let gameCounter = 100
+// gameCounter++ ;
+++gameCounter;
+
+
+console.log(gameCounter);
+
+
+
+
+
 
