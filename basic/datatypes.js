5c588b27f89aae5031121e4d26c2ddebb9a33e27
sandeep kumar <somsettysandeep@gmail.com>
2024-05-12 17:11:22 +0530

    datatypes

---
+++ basic/datatypes.js
@@ -0,0 +1,24 @@
+"user strict" ; // treat all js code as newer version
+
+
+    // alert("3+3")   // we are using node  js , not browser
+
+
+    console.log(3+3);
+
+    console.log("Sandy");
+
+
+    //
+    let name = "sandeep"
+    let age = 18
+    let isLoggedIn = true ;    
+    let state     
+                                                          //bigint
+                                                          //string => ""
+                                                          //boolean => true /false
+                                                          // null
+                                                          //undefined
+                                                          // symbol
+                                                          //object
+    console.log(typeof age,name,isLoggedIn,state )                                                      
